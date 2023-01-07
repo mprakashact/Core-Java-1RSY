b7041767005cd98832865aad84b2828430e9336c
mprakashact <121894655+mprakashact@users.noreply.github.com>
2023-01-08 00:03:52 +0530

    JAVATAMIL
    10

---
+++ JAVATAMIL/Base.java
@@ -0,0 +1,11 @@
+package JAVATAMIL;
+
+public class Base{
+      private String name="base";
+
+      public void basefun(){System.out.println("Name is " + name);}
+
+      public static void main(String[] args) {
+           System.out.println(Math.sqrt(10)); 
+      }
+}
