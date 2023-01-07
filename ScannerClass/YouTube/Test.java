a777d5dd42a451c79e4d183c395ee0b7736a9446
mprakashact <121894655+mprakashact@users.noreply.github.com>
2023-01-08 00:00:58 +0530

    ScannerClass
    7

---
+++ ScannerClass/YouTube/Test.java
@@ -0,0 +1,41 @@
+package ScannerClass.YouTube;
+
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        // System.out.println("Enter no1 ");
+        Scanner sc=new Scanner(System.in);
+        // float no1 = sc.nextFloat();
+        // System.out.println("Enter no2 ");
+        // int no2 = sc.nextInt();
+
+        // System.out.println(no2);
+        // // System.out.println(no1+no2);
+        // System.out.println("Enter your name: ");
+        // String name=sc.next();
+        // System.out.println("your name is "+name);
+
+
+        // System.out.println("Enter no1");
+        // int no1 = sc.nextInt();
+        // System.out.println("Enter no2");
+        // int no2 = sc.nextInt();
+
+        // if(no1>no2)
+        //  System.out.println("no1 is greater");
+        // else if(no2>no1)
+        //  System.out.println("no2 is greater");
+        // else
+        //  System.out.println("Both are equal");
+
+        System.err.println("error message");  
+
+        // int i=System.in.read();//returns ASCII code of 1st character  
+        // System.out.println((char)i);//will print the character  
+    }
+}
